0a06d8b253835b662425daa1962c2439ca866fb2
Cal Mosher <cal@chevro.co>
2016-07-12 23:46:43 -0700

    Add another section, update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-var gulp = require('gulp');
-var markdown = require('gulp-markdown');
+var gulp = require("gulp"),
+	markdown = require("gulp-markdown"),
+    rename = require("gulp-rename");
 
-gulp.task('markdown', function () {
-	return gulp.src('src/*.md')
+gulp.task("markdown", function () {
+	return gulp.src("src/sections/*.md")
 		.pipe(markdown())
-		.pipe(gulp.dest('dist'));
+		.pipe(rename({extname: ".php"}))
+		.pipe(gulp.dest("dist/sections"));
 });
+
+gulp.task("app", function() {
+	return gulp.src("src/index.php")
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["app", "markdown"]);
