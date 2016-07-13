e5b3dcbe99b3e680a6033cfdd25a5bb7f4ea8252
Cal Mosher <cal@chevro.co>
2016-07-13 00:08:50 -0700

    Add index

---
+++ src/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Pokemon Go Tips</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+<link rel="icon" href="/assets/favicon.png" />
+<link rel="stylesheet" href="/built/main.built.css" />
+
+</head>
+
+<body>
+
+<?
+	echo "asdf";
+?>
+
+<script src="/built/main.built.js"></script>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-80663844-1', 'auto');
+ga('send', 'pageview');
+</script>
+
+</body>
+
+</html>
---
+++ src/sections/what-is.md
@@ -0,0 +1,5 @@
+## What is Pokemon Go?
+
+You caved. You're finally ready to find out what Pokemon Go is all about. Well that's what we're here for, so let's jump right in.
+
+### The
