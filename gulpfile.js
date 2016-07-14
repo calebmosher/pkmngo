30950b42c73b5fd3fd5057384769adb16670fe5f
Cal Mosher <cal@chevro.co>
2016-07-14 00:13:18 -0700

    Add local server

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require("gulp"),
-    browserify = require("browserify"),
-    source = require("vinyl-source-stream"),
+	browserify = require("browserify"),
+	source = require("vinyl-source-stream"),
 	markdown = require("gulp-markdown"),
-    rename = require("gulp-rename"),
-    notify = require("gulp-notify"),
-    sass = require("gulp-sass");
+	rename = require("gulp-rename"),
+	notify = require("gulp-notify"),
+	sass = require("gulp-sass");
+connect = require('gulp-connect-php'),
 
 
 
@@ -21,11 +22,11 @@ gulp.task("markdown", function () {
 
 // CSS
 gulp.task("sass", function() {
-    return gulp.src("src/scss/*.scss")
-        .pipe(sass())
-        .pipe(rename("main.built.css"))
-        .pipe(gulp.dest("dist/built"))
-        .pipe(notify("Finished Sass"));
+	return gulp.src("src/scss/*.scss")
+		.pipe(sass())
+		.pipe(rename("main.built.css"))
+		.pipe(gulp.dest("dist/built"))
+		.pipe(notify("Finished Sass"));
 });
 
 gulp.task("scss", ["sass"]);
@@ -36,13 +37,22 @@ gulp.task("css", ["sass"]);
 // Javascript
 gulp.task("browserify", function() {
     return browserify("src/js/index.js")
-        .bundle()
-        .pipe(source("main.built.js"))
-        .pipe(gulp.dest("dist/built"))
-        .pipe(notify("Finished Browserify"));
+		.bundle()
+		.pipe(source("main.built.js"))
+		.pipe(gulp.dest("dist/built"))
+		.pipe(notify("Finished Browserify"));
 });
 
 gulp.task("js", ["browserify"]);
+
+
+
+// Local Server
+gulp.task("server", function() {
+	connect.server({
+		port: "9000"
+	});
+});
 
 
 
